a449052d3d4aed8be978cbab850601e9690d61bd
Anton Kasyanov <anton.kasyanov@datarobot.com>
2021-01-01 18:06:30 +0200

    sphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,47 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 #include <iostream>
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    double a = dot(r.direction(), r.direction());
+    double b = 2 * dot(r.direction(), (r.origin() - center));
+    double c = dot((center + r.origin()), (center + r.origin())) - radius * radius;
+    double discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
 
 
 int main() {
 
     // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-    const int image_width = 256;
-    const int image_height = 256;
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+
 
     // Render
 
@@ -17,15 +50,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "Progress: " << image_height - j << "/" << image_height << "\n";
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            write_color(std::cout, pixel_color);
+
         }
     }
     std::cerr << "all done\n";
