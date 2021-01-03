dcc5886d581fa0feb734fcb0932aa08fa091dd8e
Anton Kasyanov <anton.kasyanov@datarobot.com>
2021-01-03 11:03:01 +0200

    my code for materials

--- src/material.h
+++ src/material.h
@@ -24,14 +24,15 @@ class lambertian : public material {
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
-            auto scatter_direction = rec.normal + random_unit_vector();
+            attenuation = albedo;
+            vec3 scattered_point = rec.p;
+            vec3 scattered_direction = rec.normal + random_unit_vector();
 
             // Catch degenerate scatter direction
-            if (scatter_direction.near_zero())
-                scatter_direction = rec.normal;
+            if (scattered_direction.near_zero())
+                scattered_direction = rec.normal;
 
-            scattered = ray(rec.p, scatter_direction);
-            attenuation = albedo;
+            scattered = ray(scattered_point, scattered_direction);
             return true;
         }
 };
@@ -47,12 +48,15 @@ class metal : public material {
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
-            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-
-            // add a bit of randomness to the reflection
-            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
             attenuation = albedo;
-            return (dot(scattered.direction(), rec.normal) > 0);
+            vec3 scattered_point = rec.p;
+            vec3 scattered_direction = reflect(r_in.direction(), rec.normal);
+
+            // add a bit of fuzz so it is not glass-like
+            scattered_direction += fuzz * random_in_unit_sphere();
+
+            scattered = ray(scattered_point, scattered_direction);
+            return true;
         }
 
 };
